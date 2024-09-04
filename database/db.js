413f7efb9d6b0c046146b361baae6fc6f0c12f69
jangirsamarth <samarth8947@gmail.com>
2024-09-04 14:03:19 +0530

    Initial commit

---
+++ database/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// console.log("DB URI:", process.env.DB); // This should output your MongoDB URI
+
+export const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.DB, {
+     
+    });
+    console.log("Database Connected");
+  } catch (error) {
+    console.log("Error connecting to the database:", error);
+  }
+};
